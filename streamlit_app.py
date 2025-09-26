53481021f6085cc327b99d6002b723079b32374c
Håkon Holm <144897103+Hakonandreas@users.noreply.github.com>
2025-09-26 08:39:09 +0000

    test sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ st.title("Project IND320")
 st.write(
     "My Streamlit app for IND320"
 )
-
+'''
 st.sidebar.title("Navigation")
 
-'''# List of page names (must match filenames without .py)
+# List of page names (must match filenames without .py)
 pages = ["Home", "Page1", "Page2", "Page3", "Page4"]
 choice = st.sidebar.radio("Go to", pages)
 
