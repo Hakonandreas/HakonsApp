240b74b38e2fb6ec6772c916b30fd6c32eb91a30
Håkon Holm <144897103+Hakonandreas@users.noreply.github.com>
2025-09-26 08:39:53 +0000

    went back to selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ st.title("Project IND320")
 st.write(
     "My Streamlit app for IND320"
 )
-'''
+
 st.sidebar.title("Navigation")
 
 # List of page names (must match filenames without .py)
 pages = ["Home", "Page1", "Page2", "Page3", "Page4"]
-choice = st.sidebar.radio("Go to", pages)
+choice = st.sidebar.selectbox("Go to", pages)
 
 # Load selected page
 if choice == "Home":
@@ -18,4 +18,4 @@ else:
     # Dynamically run the selected page from Pages folder
     with open(f"Pages/{choice}.py", "r") as f:
         code = f.read()
-    exec(code, globals())'''
+    exec(code, globals())
