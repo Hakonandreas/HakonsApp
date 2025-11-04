e45ff59c9f2fc02a718db041e97201b3f868bb3e
Håkon Holm <144897103+Hakonandreas@users.noreply.github.com>
2025-11-04 12:11:51 +0000

    Changed the page order

--- Pages/Page 5.py
+++ Pages/Page 5.py
@@ -4,5 +4,3 @@ st.title("What about a joke while you're here?")
 # write a joke about programming
 st.write("Why do programmers prefer dark mode? Because light attracts bugs!")
 
-
-st.write("testing")
