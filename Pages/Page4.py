fcfba3b61ad88049580175eadf30c2d8925a6b93
Håkon Holm <144897103+Hakonandreas@users.noreply.github.com>
2025-11-04 11:55:50 +0000

    test

--- Pages/Page4.py
+++ Pages/Page4.py
@@ -3,3 +3,6 @@ st.title("What about a joke while you're here?")
 
 # write a joke about programming
 st.write("Why do programmers prefer dark mode? Because light attracts bugs!")
+
+
+st.write("testing")
