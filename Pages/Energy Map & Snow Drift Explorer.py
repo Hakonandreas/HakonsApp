b58381b0cc13f7d8fa161526d5c1a479f642e174
Håkon Holm <144897103+Hakonandreas@users.noreply.github.com>
2025-11-26 16:58:20 +0000

    change size

--- Pages/Energy Map & Snow Drift Explorer.py
+++ Pages/Energy Map & Snow Drift Explorer.py
@@ -204,7 +204,7 @@ if st.session_state.clicked_point:
 
     st.write("### Wind rose")
     try:
-        fig = plot_wind_rose(lat, lon, start_year, end_year, figsize=(4,4))
+        fig = plot_wind_rose(lat, lon, start_year, end_year, figsize=(2,2))
         st.pyplot(fig, clear_figure=True)
     except FileNotFoundError as e:
         st.error(str(e))
